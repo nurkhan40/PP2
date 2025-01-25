c075d9600ba11bd3665cc42506675fe859ce06b0
Nurkhan Kyrykbay <kyrykbai6@gmail.com>
2025-01-25 19:42:52 +0500

    9.2 Modify Strings Added

---
+++ lab_1/9_2_Modify_Strings.py
@@ -0,0 +1,79 @@
+"""
+Python - Modify Strings.
+
+Python has a set of built-in methods that you can use on strings.
+"""
+
+print("----------------------------------------------------------------------------------------\n")
+print("The code data type 'Upper Case'\n")
+print("The upper() method returns the string in upper case.\n")
+print("Example for Upper Case:\n")
+print('a = "Hello, World!"\n')
+print('print(a.upper())\n')
+print("The output\n")
+
+# Upper case example:
+a = "Hello, World!"
+print(a.upper())
+
+print("----------------------------------------------------------------------------------------\n")
+print("The code data type 'Lower Case'\n")
+print("The lower() method returns the string in lower case.\n")
+print("Example for Lower Case:\n")
+print('a = "Hello, World!"\n')
+print('print(a.lower())\n')
+print("The output\n")
+
+# Lower case example:
+a = "Hello, World!"
+print(a.lower())
+
+print("----------------------------------------------------------------------------------------\n")
+print("The code data type 'Remove Whitespace'\n")
+print("The strip() method removes any whitespace from the beginning or the end.\n")
+print("Example for Remove Whitespace:\n")
+print('a = " Hello, World! "\n')
+print('print(a.strip()) # returns "Hello, World!"\n')
+print("The output\n")
+
+# Remove whitespace example:
+a = " Hello, World! "
+print(a.strip())
+
+print("----------------------------------------------------------------------------------------\n")
+print("The code data type 'Remove Whitespace'\n")
+print("The strip() method removes any whitespace from the beginning or the end.\n")
+print("Example for Remove Whitespace:\n")
+print('a = " Hello, World! "\n')
+print('print(a.strip()) # returns "Hello, World!"\n')
+print("The output\n")
+
+# Remove whitespace example:
+a = " Hello, World! "
+print(a.strip())
+
+print("----------------------------------------------------------------------------------------\n")
+print("The code data type 'Replace String'\n")
+print("The replace() method replaces a string with another string.\n")
+print("Example for Replace String:\n")
+print('a = "Hello, World!"\n')
+print('print(a.replace("H", "J"))\n')
+print("The output\n")
+
+# Replace string example:
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+print("----------------------------------------------------------------------------------------\n")
+print("The code data type 'Split String'\n")
+print("The split() method returns a list where the text between the specified separator becomes the list items.\n")
+print("Example for Split String:\n")
+print('a = "Hello, World!"\n')
+print('print(a.split(",")) # returns [\'Hello\', \' World!\']\n')
+print("The output\n")
+
+# Split string example:
+a = "Hello, World!"
+print(a.split(","))
+
+print("----------------------------------------------------------------------------------------\n")
