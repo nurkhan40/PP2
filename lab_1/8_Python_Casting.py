bae9b80873b22f60c88b27ec81a4bf18040ecfb2
Nurkhan Kyrykbay <kyrykbai6@gmail.com>
2025-01-25 19:26:48 +0500

    8 Python Casting Added

---
+++ lab_1/8_Python_Casting.py
@@ -0,0 +1,62 @@
+print("The code data type 'Specify a Variable Type'\n")
+print("There may be times when you want to specify a type onto a variable. This can be done with casting. Python is an object-oriented language, and as such, it uses classes to define data types, including its primitive types.\n")
+print("Casting in Python is therefore done using constructor functions:\n")
+print("int() - constructs an integer number from an integer literal, a float literal (by removing all decimals), or a string literal (providing the string represents a whole number)\n")
+print("float() - constructs a float number from an integer literal, a float literal, or a string literal (providing the string represents a float or an integer)\n")
+print("str() - constructs a string from a wide variety of data types, including strings, integer literals, and float literals\n")
+
+print("------------------------------------------")
+
+print("Example for Integers:\n")
+print('x = int(1)   # x will be 1\n')
+print('y = int(2.8) # y will be 2\n')
+print('z = int("3") # z will be 3\n')
+
+print("The output\n")
+
+# Integers:
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+print("------------------------------------------")
+
+print("\nExample for Floats:\n")
+print('x = float(1)     # x will be 1.0\n')
+print('y = float(2.8)   # y will be 2.8\n')
+print('z = float("3")   # z will be 3.0\n')
+print('w = float("4.2") # w will be 4.2\n')
+
+print("The output\n")
+
+# Floats:
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+print("------------------------------------------")
+
+print("\nExample for Strings:\n")
+print('x = str("s1") # x will be \'s1\'\n')
+print('y = str(2)    # y will be \'2\'\n')
+print('z = str(3.0)  # z will be \'3.0\'\n')
+
+print("The output\n")
+
+# Strings:
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+
+print("------------------------------------------")
