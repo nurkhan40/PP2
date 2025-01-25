c758f85daf7a0779b626c6fa2ef8e5a8f8ea6463
Nurkhan Kyrykbay <kyrykbai6@gmail.com>
2025-01-25 20:22:23 +0500

    9.3 Concentrate Strings Added

---
+++ lab_1/9_3_Concatenate_Strings.py
@@ -0,0 +1,38 @@
+"""
+String Concatenation
+To concatenate, or combine, two strings you can use the + operator.
+"""
+
+print("----------------------------------------------------------------------------------------\n")
+print("The code data type 'Merge String'\n")
+print("Merge variable a with variable b into variable c:\n")
+print("Example for Merge String:\n")
+print('a = "Hello"\n')
+print('b = "World"\n')
+print('c = a + b\n')
+print('print(c)\n')
+print("The output\n")
+
+# Merging string example:
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+print("----------------------------------------------------------------------------------------\n")
+print("The code data type 'Merge String with Space'\n")
+print("To add a space between them, add a ' ':\n")
+print("Example for Merge String with Space:\n")
+print('a = "Hello"\n')
+print('b = "World"\n')
+print('c = a + " " + b\n')
+print('print(c)\n')
+print("The output\n")
+
+# Merging string with space example:
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+print("----------------------------------------------------------------------------------------\n")
