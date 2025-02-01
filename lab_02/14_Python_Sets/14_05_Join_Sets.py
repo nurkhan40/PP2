b9a0c540c366745da7950bd764c8ee00554c1921
Nurkhan Kyrykbay <kyrykbai6@gmail.com>
2025-02-01 11:11:54 +0500

    upd

--- lab_02/14_Python_Sets/14_05_Join_Sets.py
+++ lab_02/14_Python_Sets/14_05_Join_Sets.py
@@ -8,27 +8,27 @@ set2 = {1, 2, 3}
 set3 = set1.union(set2)
 print(set3)
 #-----------------------------------------------------
-set1 = {"a", "b", "c"}
-set2 = {1, 2, 3}
+set11 = {"a", "b", "c"}
+set21 = {1, 2, 3}
 
-set3 = set1 | set2
-print(set3)
+set31 = set11 | set21
+print(set31)
 #-----------------------------------------------------
-set1 = {"a", "b", "c"}
-set2 = {1, 2, 3}
-set3 = {"John", "Elena"}
-set4 = {"apple", "bananas", "cherry"}
+set12 = {"a", "b", "c"}
+set22 = {1, 2, 3}
+set32 = {"John", "Elena"}
+set42 = {"apple", "bananas", "cherry"}
 
-myset = set1.union(set2, set3, set4)
+myset = set1.union(set22, set32, set42)
 print(myset)
 #-----------------------------------------------------
-set1 = {"a", "b", "c"}
-set2 = {1, 2, 3}
-set3 = {"John", "Elena"}
-set4 = {"apple", "bananas", "cherry"}
+set1q = {"a", "b", "c"}
+set2q = {1, 2, 3}
+set3q = {"John", "Elena"}
+set4q = {"apple", "bananas", "cherry"}
 
-myset = set1 | set2 | set3 |set4
-print(myset)
+mysetq = set1q | set2q | set3q |set4q
+print(mysetq)
 #-----------------------------------------------------
 x = {"a", "b", "c"}
 y = (1, 2, 3)
@@ -36,75 +36,75 @@ y = (1, 2, 3)
 z = x.union(y)
 print(z)
 #-----------------------------------------------------
-set1 = {"a", "b" , "c"}
-set2 = {1, 2, 3}
+set1w = {"a", "b" , "c"}
+set2w = {1, 2, 3}
 
-set1.update(set2)
-print(set1)
+set1w.update(set2w)
+print(set1w)
 #-----------------------------------------------------
-set1 = {"apple", "banana", "cherry"}
-set2 = {"google", "microsoft", "apple"}
+set1a = {"apple", "banana", "cherry"}
+set2a = {"google", "microsoft", "apple"}
 
-set3 = set1.intersection(set2)
-print(set3)
+set3a = set1a.intersection(set2a)
+print(set3a)
 #-----------------------------------------------------
-set1 = {"apple", "banana", "cherry"}
-set2 = {"google", "microsoft", "apple"}
+set1s = {"apple", "banana", "cherry"}
+set2s = {"google", "microsoft", "apple"}
 
-set3 = set1 & set2
-print(set3)
+set3s = set1s & set2s
+print(set3s)
 #-----------------------------------------------------
-set1 = {"apple", "banana", "cherry"}
-set2 = {"google", "microsoft", "apple"}
+set1e = {"apple", "banana", "cherry"}
+set2e = {"google", "microsoft", "apple"}
 
-set1.intersection_update(set2)
+set1e.intersection_update(set2e)
 
-print(set1)
+print(set1e)
 #-----------------------------------------------------
-set1 = {"apple", 1,  "banana", 0, "cherry"}
-set2 = {False, "google", 1, "apple", 2, True}
+set1z = {"apple", 1,  "banana", 0, "cherry"}
+set2z = {False, "google", 1, "apple", 2, True}
 
-set3 = set1.intersection(set2)
+set3z = set1z.intersection(set2z)
 
-print(set3)
+print(set3z)
 #-----------------------------------------------------
-set1 = {"apple", "banana", "cherry"}
-set2 = {"google", "microsoft", "apple"}
+set1x = {"apple", "banana", "cherry"}
+set2x = {"google", "microsoft", "apple"}
 
-set3 = set1.difference(set2)
+set3x = set1x.difference(set2x)
 
-print(set3)
+print(set3x)
 #-----------------------------------------------------
-set1 = {"apple", "banana", "cherry"}
-set2 = {"google", "microsoft", "apple"}
+set13 = {"apple", "banana", "cherry"}
+set23 = {"google", "microsoft", "apple"}
 
-set3 = set1 - set2
-print(set3)
+set33 = set13 - set23
+print(set33)
 #-----------------------------------------------------
-set1 = {"apple", "banana", "cherry"}
-set2 = {"google", "microsoft", "apple"}
+set1h = {"apple", "banana", "cherry"}
+set2h = {"google", "microsoft", "apple"}
 
-set1.difference_update(set2)
+set1h.difference_update(set2h)
 
-print(set1)
+print(set1h)
 #-----------------------------------------------------
-set1 = {"apple", "banana", "cherry"}
-set2 = {"google", "microsoft", "apple"}
+set1v = {"apple", "banana", "cherry"}
+set2v = {"google", "microsoft", "apple"}
 
-set3 = set1.symmetric_difference(set2)
+set3v = set1v.symmetric_difference(set2v)
 
-print(set3)
+print(set3v)
 #-----------------------------------------------------
-set1 = {"apple", "banana", "cherry"}
-set2 = {"google", "microsoft", "apple"}
+set1l = {"apple", "banana", "cherry"}
+set2l = {"google", "microsoft", "apple"}
 
-set3 = set1 ^ set2
-print(set3)
+set3l = set1l ^ set2l
+print(set3l)
 #-----------------------------------------------------
-set1 = {"apple", "banana", "cherry"}
-set2 = {"google", "microsoft", "apple"}
+set1k = {"apple", "banana", "cherry"}
+set2k = {"google", "microsoft", "apple"}
 
-set1.symmetric_difference_update(set2)
+set1k.symmetric_difference_update(set2k)
 
-print(set1)
+print(set1k)
 #-----------------------------------------------------
