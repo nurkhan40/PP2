1cfe3182c5532ce9a3632a2163dfd6d5179c187d
Nurkhan Kyrykbay <kyrykbai6@gmail.com>
2025-02-01 15:18:27 +0500

    updated

--- lab_03/19_Python_Functions.py
+++ lab_03/19_Python_Functions.py
@@ -133,3 +133,157 @@ print("Recursion Example Results:")
 tri_recursionx(6)
 
 #-----------------------------------------------------
+
+
+print("----------------------------------------------------------------------------------------")
+print('Python Functions')
+
+print("----------------------------------------------------------------------------------------")
+print('Example 1: Defining a Function')
+
+def my_function():
+    print("Hello from a function")
+print('def my_function():')
+print('    print("Hello from a function")')
+
+print("----------------------------------------------------------------------------------------")
+print('Example 2: Calling a Function')
+
+def my_function1():
+    print("Hello from a function")
+
+my_function1()
+print('def my_function1():')
+print('    print("Hello from a function")')
+print('my_function1()')
+
+print("----------------------------------------------------------------------------------------")
+print('Example 3: Function with Parameters')
+
+def my_function2(fname):
+    print(fname + " Refsnes")
+
+my_function2("Emil")
+my_function2("Tobias")
+my_function2("Linus")
+print('def my_function2(fname):')
+print('    print(fname + " Refsnes")')
+print('my_function2("Emil")')
+print('my_function2("Tobias")')
+print('my_function2("Linus")')
+
+print("----------------------------------------------------------------------------------------")
+print('Example 4: Function with Two Parameters')
+
+def my_function3(fname, lname):
+    print(fname + " " + lname)
+
+my_function3("Emil", "Refsnes")
+print('def my_function3(fname, lname):')
+print('    print(fname + " " + lname)')
+print('my_function3("Emil", "Refsnes")')
+
+print("----------------------------------------------------------------------------------------")
+print('Example 5: Function with Arbitrary Arguments')
+
+def my_function4(*kids):
+    print("The youngest child is " + kids[2])
+
+my_function4("Emil", "Tobias", "Linus")
+print('def my_function4(*kids):')
+print('    print("The youngest child is " + kids[2])')
+print('my_function4("Emil", "Tobias", "Linus")')
+
+print("----------------------------------------------------------------------------------------")
+print('Example 6: Function with Keyword Arguments')
+
+def my_function5(child3, child2, child1):
+    print("The youngest child is " + child3)
+
+my_function5(child1="Emil", child2="Tobias", child3="Linus")
+print('def my_function5(child3, child2, child1):')
+print('    print("The youngest child is " + child3)')
+print('my_function5(child1="Emil", child2="Tobias", child3="Linus")')
+
+print("----------------------------------------------------------------------------------------")
+print('Example 7: Function with Arbitrary Keyword Arguments')
+
+def my_function6(**kid):
+    print("His last name is " + kid["lname"])
+
+my_function6(fname="Tobias", lname="Refsnes")
+print('def my_function6(**kid):')
+print('    print("His last name is " + kid["lname"])')
+print('my_function6(fname="Tobias", lname="Refsnes")')
+
+print("----------------------------------------------------------------------------------------")
+print('Example 8: Function with Default Parameter Value')
+
+def my_function7(country="Norway"):
+    print("I am from " + country)
+
+my_function7("Sweden")
+my_function7("India")
+my_function7()
+my_function7("Brazil")
+print('def my_function7(country="Norway"):')
+print('    print("I am from " + country)')
+print('my_function7("Sweden")')
+print('my_function7("India")')
+print('my_function7()')
+print('my_function7("Brazil")')
+
+print("----------------------------------------------------------------------------------------")
+print('Example 9: Function with a List Parameter')
+
+def my_function8(food):
+    for x in food:
+        print(x)
+
+fruits = ["apple", "banana", "cherry"]
+my_function8(fruits)
+print('def my_function8(food):')
+print('    for x in food:')
+print('        print(x)')
+print('fruits = ["apple", "banana", "cherry"]')
+print('my_function8(fruits)')
+
+print("----------------------------------------------------------------------------------------")
+print('Example 10: Function with a Return Value')
+
+def my_function9(x):
+    return 5 * x
+
+print(my_function9(3))
+print(my_function9(5))
+print(my_function9(9))
+print('def my_function9(x):')
+print('    return 5 * x')
+print('print(my_function9(3))')
+print('print(my_function9(5))')
+print('print(my_function9(9))')
+
+print("----------------------------------------------------------------------------------------")
+print('Example 11: Function with Recursion')
+
+def tri_recursionx(k):
+    if k > 0:
+        result = k + tri_recursionx(k - 1)
+        print(result)
+    else:
+        result = 0
+    return result
+
+print("Recursion Example Results:")
+tri_recursionx(6)
+print('def tri_recursionx(k):')
+print('    if k > 0:')
+print('        result = k + tri_recursionx(k - 1)')
+print('        print(result)')
+print('    else:')
+print('        result = 0')
+print('    return result')
+print('print("Recursion Example Results:")')
+print('tri_recursionx(6)')
+
+print("----------------------------------------------------------------------------------------")
--- lab_03/20_Python_Lambda.py
+++ lab_03/20_Python_Lambda.py
@@ -1,6 +1,7 @@
 """
 Python Lambda
 """
+"""
 #-----------------------------------------------------
 x = lambda a : a + 10
 print(x(5))
@@ -34,3 +35,73 @@ mytripler1 = myfunc2(3)
 print(mydoubler1(11))
 print(mytripler1(11))
 #-----------------------------------------------------
+"""
+
+print("----------------------------------------------------------------------------------------")
+print('Python Lambda')
+
+print("----------------------------------------------------------------------------------------")
+print('Example 1: Simple Lambda Function')
+
+x = lambda a: a + 10
+print('x = lambda a: a + 10')
+print('print(x(5))')
+print(x(5))
+
+print("----------------------------------------------------------------------------------------")
+print('Example 2: Lambda with Two Arguments')
+
+x1 = lambda a, b: a * b
+print('x1 = lambda a, b: a * b')
+print('print(x1(5, 6))')
+print(x1(5, 6))
+
+print("----------------------------------------------------------------------------------------")
+print('Example 3: Lambda with Three Arguments')
+
+x2 = lambda a, b, c: a + b + c
+print('x2 = lambda a, b, c: a + b + c')
+print('print(x2(5, 6, 2))')
+print(x2(5, 6, 2))
+
+print("----------------------------------------------------------------------------------------")
+print('Example 4: Using Lambda in a Function')
+
+def myfunc(n):
+    return lambda a: a * n
+
+mydoubler = myfunc(2)
+print('def myfunc(n): return lambda a: a * n')
+print('mydoubler = myfunc(2)')
+print('print(mydoubler(11))')
+print(mydoubler(11))
+
+print("----------------------------------------------------------------------------------------")
+print('Example 5: Tripling with Lambda')
+
+def myfunc1(n):
+    return lambda a: a * n
+
+mytripler = myfunc1(3)
+print('def myfunc1(n): return lambda a: a * n')
+print('mytripler = myfunc1(3)')
+print('print(mytripler(11))')
+print(mytripler(11))
+
+print("----------------------------------------------------------------------------------------")
+print('Example 6: Doubling and Tripling Using Lambda')
+
+def myfunc2(n):
+    return lambda a: a * n
+
+mydoubler1 = myfunc2(2)
+mytripler1 = myfunc2(3)
+print('def myfunc2(n): return lambda a: a * n')
+print('mydoubler1 = myfunc2(2)')
+print('mytripler1 = myfunc2(3)')
+print('print(mydoubler1(11))')
+print('print(mytripler1(11))')
+print(mydoubler1(11))
+print(mytripler1(11))
+
+print("----------------------------------------------------------------------------------------")
