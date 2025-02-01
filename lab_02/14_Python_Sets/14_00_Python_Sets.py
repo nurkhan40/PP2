25231a5f7f95386eef42aa33df2f4a0656a37e63
Nurkhan Kyrykbay <kyrykbai6@gmail.com>
2025-02-01 11:04:23 +0500

    Added 14 Python Sets

--- lab_02/14_Python_Sets/14_00_Python_Sets.py
+++ lab_02/14_Python_Sets/14_00_Python_Sets.py
@@ -1,20 +1,37 @@
 """
-
+Python Sets
 """
 #-----------------------------------------------------
-
+myset = {"apple", "banana", "cherry"}
 #-----------------------------------------------------
-
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
 #-----------------------------------------------------
+thisset1 = {"apple", "banana", "cherry", "apple"}
 
+print(thisset1)
 #-----------------------------------------------------
+thisset2 = {"apple", "banana", "cherry", True, 1, 2}
 
+print(thisset2)
 #-----------------------------------------------------
+thisset3 = {"apple", "banana", "cherry", False, True, 0}
 
+print(thisset3)
 #-----------------------------------------------------
+thisset4 = {"apple", "banana", "cherry"}
 
+print(len(thisset4))
 #-----------------------------------------------------
-
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
 #-----------------------------------------------------
-
+set4 = {"abc", 34, True, 40, "male"}
+#-----------------------------------------------------
+myset1 = {"apple", "banana", "cherry"}
+print(type(myset))
+#-----------------------------------------------------
+thisset1 = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset1)
 #-----------------------------------------------------
--- lab_02/14_Python_Sets/14_03_Remove_Set_Items.py
+++ lab_02/14_Python_Sets/14_03_Remove_Set_Items.py
@@ -1,20 +1,36 @@
 """
-
+Python - Remove Set Items
 """
 #-----------------------------------------------------
+thisset = {"apple", "banana", "cherry"}
 
+thisset.remove("banana")
+
+print(thisset)
 #-----------------------------------------------------
+thissetq = {"apple", "banana", "cherry"}
 
+thissetq.discard("banana")
+
+print(thissetq)
 #-----------------------------------------------------
+thisset1 = {"apple", "banana", "cherry"}
 
+x = thisset1.pop()
+
+print(x)
+
+print(thisset1)
 #-----------------------------------------------------
+thisset2 = {"apple", "banana", "cherry"}
 
+thisset2.clear()
+
+print(thisset2)
 #-----------------------------------------------------
+thisset3 = {"apple", "banana", "cherry"}
 
-#-----------------------------------------------------
+del thisset3
 
-#-----------------------------------------------------
-
-#-----------------------------------------------------
-
+print(thisset3)
 #-----------------------------------------------------
